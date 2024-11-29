494a7a841dcf51b038e0581a229115507a8e0908
Tanvi Agrawal <1998tanvi@gmail.com>
2024-11-29 18:16:21 +0530

    marketing config commit

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -15,7 +15,7 @@ const prodConfig = {
         new ModuleFedrationPlugin({
             name: 'container',
             remotes: {
-                marketing: `marketing@${domain}/marketing/remoteEntry.js`
+                marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`
             },
             shared: packageJson.dependencies
         })
