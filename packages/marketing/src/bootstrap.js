7320c9eff8240e107351ae0c2694527edbaf5106
Tanvi Agrawal <1998tanvi@gmail.com>
2024-11-30 01:12:49 +0530

    commit

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -31,7 +31,7 @@ if(process.env.NODE_ENV === 'development')
 {
     const devRoot = document.querySelector('#_marketing-dev-root');
     if(devRoot){
-        mount(devRoot, { defaultHistory: createBrowserHistory });
+        mount(devRoot, { defaultHistory: createBrowserHistory() });
     }
 }
 
